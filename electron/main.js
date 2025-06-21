b86b16909f657839ec86f9b8286d1fc7b32033da
jyarling <41969813+jyarling@users.noreply.github.com>
2025-06-20 17:27:17 -0700

    Initialize electron + react app

---
+++ electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = process.env.NODE_ENV === 'development';
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+  } else {
+    win.loadFile(path.join(__dirname, '../app/dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
