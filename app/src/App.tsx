b6b1e36f7e612f4c803d904edd716b0f7a425b86
jyarling <41969813+jyarling@users.noreply.github.com>
2025-06-20 18:54:30 -0700

    Add basic ACARS table UI

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,55 @@
-import './App.css'
+import "./App.css";
+
+interface AcarsMessage {
+  id: number;
+  time: string;
+  sender: string;
+  message: string;
+}
+
+const dummyData: AcarsMessage[] = [
+  {
+    id: 1,
+    time: "12:00Z",
+    sender: "ATC",
+    message: "CLEARED FL350"
+  },
+  {
+    id: 2,
+    time: "12:05Z",
+    sender: "DISP",
+    message: "WEATHER UPDATE"
+  },
+  {
+    id: 3,
+    time: "12:10Z",
+    sender: "ATC",
+    message: "CONTACT CENTER"
+  }
+];
 
 function App() {
   return (
     <div className="App">
       <h1>Electron ACARS Viewer</h1>
-      <p>ACARS data will appear here.</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Sender</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dummyData.map((item) => (
+            <tr key={item.id}>
+              <td>{item.time}</td>
+              <td>{item.sender}</td>
+              <td>{item.message}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
